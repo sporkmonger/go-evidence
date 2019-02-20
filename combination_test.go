daaa03d1f630829f7495721734b0ad962643f760
Bob Aman <bob@sporkmonger.com>
2019-02-19 15:14:35 -1000

    Add tests for conflict

--- combination_test.go
+++ combination_test.go
@@ -30,16 +30,54 @@ func TestCombineConjunctive(t *testing.T) {
 	mf2.Set(K("yellow", "green"), 0.01)
 	mf2.Set(K("red", "yellow", "green"), 0.3)
 
-	cf := CombineConjunctive(mf1, mf2)
-	assert.InDelta(0.0, cf.Get(K()), tolerance)
-	assert.InDelta(0.32737, cf.Get(K("red")), tolerance)
-	assert.InDelta(0.35403, cf.Get(K("yellow")), tolerance)
-	assert.InDelta(0.18659, cf.Get(K("green")), tolerance)
-	assert.InDelta(0.03639, cf.Get(K("red", "yellow")), tolerance)
-	assert.InDelta(0.02634, cf.Get(K("red", "green")), tolerance)
-	assert.InDelta(0.02139, cf.Get(K("yellow", "green")), tolerance)
-	assert.InDelta(0.04789, cf.Get(K("red", "yellow", "green")), tolerance)
-	assert.True(cf.Valid())
+	cf1 := CombineConjunctive(mf1, mf2)
+	assert.InDelta(0.0, cf1.Get(K()), tolerance)
+	assert.InDelta(0.32737, cf1.Get(K("red")), tolerance)
+	assert.InDelta(0.35403, cf1.Get(K("yellow")), tolerance)
+	assert.InDelta(0.18659, cf1.Get(K("green")), tolerance)
+	assert.InDelta(0.03639, cf1.Get(K("red", "yellow")), tolerance)
+	assert.InDelta(0.02634, cf1.Get(K("red", "green")), tolerance)
+	assert.InDelta(0.02139, cf1.Get(K("yellow", "green")), tolerance)
+	assert.InDelta(0.04789, cf1.Get(K("red", "yellow", "green")), tolerance)
+	assert.True(cf1.Valid())
+
+	mf3 := &MassFunction{}
+	mf3.Set(K(), 0.0)
+	mf3.Set(K("allow"), 0.35)
+	mf3.Set(K("deny"), 0.2)
+	mf3.Set(K("allow", "deny"), 0.45)
+
+	mf4 := &MassFunction{}
+	mf4.Set(K(), 0.0)
+	mf4.Set(K("allow"), 0.0)
+	mf4.Set(K("deny"), 0.0)
+	mf4.Set(K("allow", "deny"), 1.0)
+
+	cf2 := CombineConjunctive(mf3, mf4)
+	assert.InDelta(0.0, cf2.Get(K()), tolerance)
+	assert.InDelta(0.35, cf2.Get(K("allow")), tolerance)
+	assert.InDelta(0.2, cf2.Get(K("deny")), tolerance)
+	assert.InDelta(0.45, cf2.Get(K("allow", "deny")), tolerance)
+	assert.True(cf2.Valid())
+
+	mf5 := &MassFunction{}
+	mf5.Set(K(), 0.0)
+	mf5.Set(K("allow"), 0.99) // NaN if 1.0
+	mf5.Set(K("deny"), 0.01)
+	mf5.Set(K("allow", "deny"), 0.0)
+
+	mf6 := &MassFunction{}
+	mf6.Set(K(), 0.0)
+	mf6.Set(K("allow"), 0.01)
+	mf6.Set(K("deny"), 0.99) // NaN if 1.0
+	mf6.Set(K("allow", "deny"), 0.0)
+
+	cf3 := CombineConjunctive(mf5, mf6)
+	assert.InDelta(0.0, cf3.Get(K()), tolerance)
+	assert.InDelta(0.5, cf3.Get(K("allow")), tolerance)
+	assert.InDelta(0.5, cf3.Get(K("deny")), tolerance)
+	assert.InDelta(0.0, cf3.Get(K("allow", "deny")), tolerance)
+	assert.True(cf3.Valid())
 }
 
 func TestCombineDisjunctive(t *testing.T) {
@@ -66,14 +104,52 @@ func TestCombineDisjunctive(t *testing.T) {
 	mf2.Set(K("yellow", "green"), 0.01)
 	mf2.Set(K("red", "yellow", "green"), 0.3)
 
-	cf := CombineDisjunctive(mf1, mf2)
-	assert.InDelta(0.0, cf.Get(K()), tolerance)
-	assert.InDelta(0.0525, cf.Get(K("red")), tolerance)
-	assert.InDelta(0.075, cf.Get(K("yellow")), tolerance)
-	assert.InDelta(0.03, cf.Get(K("green")), tolerance)
-	assert.InDelta(0.1893, cf.Get(K("red", "yellow")), tolerance)
-	assert.InDelta(0.1155, cf.Get(K("red", "green")), tolerance)
-	assert.InDelta(0.1194, cf.Get(K("yellow", "green")), tolerance)
-	assert.InDelta(0.4183, cf.Get(K("red", "yellow", "green")), tolerance)
-	assert.True(cf.Valid())
+	cf1 := CombineDisjunctive(mf1, mf2)
+	assert.InDelta(0.0, cf1.Get(K()), tolerance)
+	assert.InDelta(0.0525, cf1.Get(K("red")), tolerance)
+	assert.InDelta(0.075, cf1.Get(K("yellow")), tolerance)
+	assert.InDelta(0.03, cf1.Get(K("green")), tolerance)
+	assert.InDelta(0.1893, cf1.Get(K("red", "yellow")), tolerance)
+	assert.InDelta(0.1155, cf1.Get(K("red", "green")), tolerance)
+	assert.InDelta(0.1194, cf1.Get(K("yellow", "green")), tolerance)
+	assert.InDelta(0.4183, cf1.Get(K("red", "yellow", "green")), tolerance)
+	assert.True(cf1.Valid())
+
+	mf3 := &MassFunction{}
+	mf3.Set(K(), 0.0)
+	mf3.Set(K("allow"), 0.35)
+	mf3.Set(K("deny"), 0.2)
+	mf3.Set(K("allow", "deny"), 0.45)
+
+	mf4 := &MassFunction{}
+	mf4.Set(K(), 0.0)
+	mf4.Set(K("allow"), 0.0)
+	mf4.Set(K("deny"), 0.0)
+	mf4.Set(K("allow", "deny"), 1.0)
+
+	cf2 := CombineDisjunctive(mf3, mf4)
+	assert.InDelta(0.0, cf2.Get(K()), tolerance)
+	assert.InDelta(0.0, cf2.Get(K("allow")), tolerance)
+	assert.InDelta(0.0, cf2.Get(K("deny")), tolerance)
+	assert.InDelta(1.0, cf2.Get(K("allow", "deny")), tolerance)
+	assert.True(cf2.Valid())
+
+	mf5 := &MassFunction{}
+	mf5.Set(K(), 0.0)
+	mf5.Set(K("allow"), 1.0)
+	mf5.Set(K("deny"), 0.0)
+	mf5.Set(K("allow", "deny"), 0.0)
+
+	mf6 := &MassFunction{}
+	mf6.Set(K(), 0.0)
+	mf6.Set(K("allow"), 0.0)
+	mf6.Set(K("deny"), 1.0)
+	mf6.Set(K("allow", "deny"), 0.0)
+
+	cf3 := CombineDisjunctive(mf5, mf6)
+	assert.InDelta(0.0, cf3.Get(K()), tolerance)
+	assert.InDelta(0.0, cf3.Get(K("allow")), tolerance)
+	assert.InDelta(0.0, cf3.Get(K("deny")), tolerance)
+	assert.InDelta(1.0, cf3.Get(K("allow", "deny")), tolerance)
+	assert.True(cf3.Valid())
 }
