8627093208d9f269693dafc2226e3514a3334c43
Bob Aman <bob@sporkmonger.com>
2019-02-24 21:10:47 -1000

    Add Murphy average combination method.

--- combination.go
+++ combination.go
@@ -64,3 +64,28 @@ func pairwiseCombineDisjunctive(mf1 *MassFunction, mf2 *MassFunction) (cf *MassF
 	}
 	return cf
 }
+
+// CombineMurphyAverage takes two or more MassFunctions and returns a new
+// MassFunction according to Murphy's rule of combination, first averaging
+// the masses and then performing a conjunctive combination. Returns
+// nil if no MassFunctions are provided.
+func CombineMurphyAverage(mfns ...*MassFunction) (cf *MassFunction) {
+	if len(mfns) == 0 {
+		return nil
+	}
+	count := len(mfns)
+	cf = &MassFunction{}
+	cf.init()
+	for _, p1 := range mfns[0].Powerset() {
+		sum := 0.0
+		for _, mf := range mfns {
+			sum += mf.getUnsafe(p1)
+		}
+		cf.Set(p1, sum/float64(count))
+	}
+	cfRepeat := make([]*MassFunction, count)
+	for i := 0; i < count; i++ {
+		cfRepeat[i] = cf
+	}
+	return CombineConjunctive(cfRepeat...)
+}
--- combination_test.go
+++ combination_test.go
@@ -417,6 +417,270 @@ func TestCombineDisjunctive(t *testing.T) {
 	}
 }
 
+func TestCombineMurphyAverage(t *testing.T) {
+	const tolerance = 0.0025
+
+	tcs := []struct {
+		name        string
+		mfns        func() []*MassFunction
+		expectedMfn func() *MassFunction
+	}{
+		{
+			name: "traffic light",
+			mfns: func() []*MassFunction {
+				mf1 := &MassFunction{}
+				mf1.Set(K(), 0.0)
+				mf1.Set(K("red"), 0.35)
+				mf1.Set(K("yellow"), 0.25)
+				mf1.Set(K("green"), 0.15)
+				mf1.Set(K("red", "yellow"), 0.06)
+				mf1.Set(K("red", "green"), 0.05)
+				mf1.Set(K("yellow", "green"), 0.04)
+				mf1.Set(K("red", "yellow", "green"), 0.1)
+
+				mf2 := &MassFunction{}
+				mf2.Set(K(), 0.0)
+				mf2.Set(K("red"), 0.15)
+				mf2.Set(K("yellow"), 0.3)
+				mf2.Set(K("green"), 0.2)
+				mf2.Set(K("red", "yellow"), 0.03)
+				mf2.Set(K("red", "green"), 0.01)
+				mf2.Set(K("yellow", "green"), 0.01)
+				mf2.Set(K("red", "yellow", "green"), 0.3)
+
+				return []*MassFunction{mf1, mf2}
+			},
+			expectedMfn: func() *MassFunction {
+				cf := &MassFunction{}
+				cf.Set(K(), 0.0)
+				cf.Set(K("red"), 0.31973)
+				cf.Set(K("yellow"), 0.35711)
+				cf.Set(K("green"), 0.19144)
+				cf.Set(K("red", "yellow"), 0.03159)
+				cf.Set(K("red", "green"), 0.02035)
+				cf.Set(K("yellow", "green"), 0.01677)
+				cf.Set(K("red", "yellow", "green"), 0.06309)
+				return cf
+			},
+		},
+		{
+			name: "block decision factor out mass function",
+			mfns: func() []*MassFunction {
+				mf1 := &MassFunction{}
+				mf1.Set(K(), 0.0)
+				mf1.Set(K("allow"), 0.35)
+				mf1.Set(K("deny"), 0.2)
+				mf1.Set(K("allow", "deny"), 0.45)
+
+				mf2 := &MassFunction{}
+				mf2.Set(K(), 0.0)
+				mf2.Set(K("allow"), 0.0)
+				mf2.Set(K("deny"), 0.0)
+				mf2.Set(K("allow", "deny"), 1.0)
+
+				return []*MassFunction{mf1, mf2}
+			},
+			expectedMfn: func() *MassFunction {
+				cf := &MassFunction{}
+				cf.Set(K(), 0.0)
+				cf.Set(K("allow"), 0.29469)
+				cf.Set(K("deny"), 0.16062)
+				cf.Set(K("allow", "deny"), 0.54469)
+				return cf
+			},
+		},
+		{
+			name: "block decision high conflict",
+			mfns: func() []*MassFunction {
+				mf1 := &MassFunction{}
+				mf1.Set(K(), 0.0)
+				mf1.Set(K("allow"), 1.0)
+				mf1.Set(K("deny"), 0.0)
+				mf1.Set(K("allow", "deny"), 0.0)
+
+				mf2 := &MassFunction{}
+				mf2.Set(K(), 0.0)
+				mf2.Set(K("allow"), 0.0)
+				mf2.Set(K("deny"), 1.0)
+				mf2.Set(K("allow", "deny"), 0.0)
+
+				return []*MassFunction{mf1, mf2}
+			},
+			expectedMfn: func() *MassFunction {
+				cf := &MassFunction{}
+				cf.Set(K(), 0.0)
+				cf.Set(K("allow"), 0.5)
+				cf.Set(K("deny"), 0.5)
+				cf.Set(K("allow", "deny"), 0.0)
+				return cf
+			},
+		},
+		{
+			// https://www.mdpi.com/1424-8220/18/5/1487/pdf
+			name: "multi-sensor target recognition system 2x evidence",
+			mfns: func() []*MassFunction {
+				mf1 := &MassFunction{}
+				mf1.Set(K(), 0.0)
+				mf1.Set(K("a"), 0.30)
+				mf1.Set(K("b"), 0.20)
+				mf1.Set(K("c"), 0.10)
+				mf1.Set(K("a", "b", "c"), 0.40)
+
+				mf2 := &MassFunction{}
+				mf2.Set(K("a"), 0.00)
+				mf2.Set(K("b"), 0.90)
+				mf2.Set(K("c"), 0.10)
+				mf2.Set(K("a", "b", "c"), 0.00)
+
+				return []*MassFunction{mf1, mf2}
+			},
+			expectedMfn: func() *MassFunction {
+				cf := &MassFunction{}
+				cf.Set(K("a"), 0.1187)
+				cf.Set(K("b"), 0.7518)
+				cf.Set(K("c"), 0.0719)
+				cf.Set(K("a", "b", "c"), 0.0576)
+				return cf
+			},
+		},
+		{
+			// https://www.mdpi.com/1424-8220/18/5/1487/pdf
+			name: "multi-sensor target recognition system 3x evidence",
+			mfns: func() []*MassFunction {
+				mf1 := &MassFunction{}
+				mf1.Set(K(), 0.0)
+				mf1.Set(K("a"), 0.30)
+				mf1.Set(K("b"), 0.20)
+				mf1.Set(K("c"), 0.10)
+				mf1.Set(K("a", "b", "c"), 0.40)
+
+				mf2 := &MassFunction{}
+				mf2.Set(K("a"), 0.00)
+				mf2.Set(K("b"), 0.90)
+				mf2.Set(K("c"), 0.10)
+				mf2.Set(K("a", "b", "c"), 0.00)
+
+				mf3 := &MassFunction{}
+				mf3.Set(K("a"), 0.60)
+				mf3.Set(K("b"), 0.10)
+				mf3.Set(K("c"), 0.10)
+				mf3.Set(K("a", "b", "c"), 0.20)
+
+				return []*MassFunction{mf1, mf2, mf3}
+			},
+			expectedMfn: func() *MassFunction {
+				cf := &MassFunction{}
+				cf.Set(K("a"), 0.3324)
+				cf.Set(K("b"), 0.5909)
+				cf.Set(K("c"), 0.0540)
+				cf.Set(K("a", "b", "c"), 0.0227)
+				return cf
+			},
+		},
+		{
+			// https://www.mdpi.com/1424-8220/18/5/1487/pdf
+			name: "multi-sensor target recognition system 4x evidence",
+			mfns: func() []*MassFunction {
+				mf1 := &MassFunction{}
+				mf1.Set(K(), 0.0)
+				mf1.Set(K("a"), 0.30)
+				mf1.Set(K("b"), 0.20)
+				mf1.Set(K("c"), 0.10)
+				mf1.Set(K("a", "b", "c"), 0.40)
+
+				mf2 := &MassFunction{}
+				mf2.Set(K("a"), 0.00)
+				mf2.Set(K("b"), 0.90)
+				mf2.Set(K("c"), 0.10)
+				mf2.Set(K("a", "b", "c"), 0.00)
+
+				mf3 := &MassFunction{}
+				mf3.Set(K("a"), 0.60)
+				mf3.Set(K("b"), 0.10)
+				mf3.Set(K("c"), 0.10)
+				mf3.Set(K("a", "b", "c"), 0.20)
+
+				mf4 := &MassFunction{}
+				mf4.Set(K("a"), 0.70)
+				mf4.Set(K("b"), 0.10)
+				mf4.Set(K("c"), 0.10)
+				mf4.Set(K("a", "b", "c"), 0.10)
+
+				return []*MassFunction{mf1, mf2, mf3, mf4}
+			},
+			expectedMfn: func() *MassFunction {
+				cf := &MassFunction{}
+				cf.Set(K("a"), 0.6170)
+				cf.Set(K("b"), 0.3505)
+				cf.Set(K("c"), 0.0272)
+				cf.Set(K("a", "b", "c"), 0.0053)
+				return cf
+			},
+		},
+		{
+			// https://www.mdpi.com/1424-8220/18/5/1487/pdf
+			name: "multi-sensor target recognition system 5x evidence",
+			mfns: func() []*MassFunction {
+				mf1 := &MassFunction{}
+				mf1.Set(K(), 0.0)
+				mf1.Set(K("a"), 0.30)
+				mf1.Set(K("b"), 0.20)
+				mf1.Set(K("c"), 0.10)
+				mf1.Set(K("a", "b", "c"), 0.40)
+
+				mf2 := &MassFunction{}
+				mf2.Set(K("a"), 0.00)
+				mf2.Set(K("b"), 0.90)
+				mf2.Set(K("c"), 0.10)
+				mf2.Set(K("a", "b", "c"), 0.00)
+
+				mf3 := &MassFunction{}
+				mf3.Set(K("a"), 0.60)
+				mf3.Set(K("b"), 0.10)
+				mf3.Set(K("c"), 0.10)
+				mf3.Set(K("a", "b", "c"), 0.20)
+
+				mf4 := &MassFunction{}
+				mf4.Set(K("a"), 0.70)
+				mf4.Set(K("b"), 0.10)
+				mf4.Set(K("c"), 0.10)
+				mf4.Set(K("a", "b", "c"), 0.10)
+
+				mf5 := &MassFunction{}
+				mf5.Set(K("a"), 0.70)
+				mf5.Set(K("b"), 0.10)
+				mf5.Set(K("c"), 0.10)
+				mf5.Set(K("a", "b", "c"), 0.10)
+
+				return []*MassFunction{mf1, mf2, mf3, mf4, mf5}
+			},
+			expectedMfn: func() *MassFunction {
+				cf := &MassFunction{}
+				cf.Set(K("a"), 0.8389)
+				cf.Set(K("b"), 0.1502)
+				cf.Set(K("c"), 0.0099)
+				cf.Set(K("a", "b", "c"), 0.0010)
+				return cf
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+			mfns := tc.mfns()
+			expectedMfn := tc.expectedMfn()
+			cf := CombineMurphyAverage(mfns...)
+			for _, possibility := range cf.Possibilities() {
+				expectedValue := expectedMfn.Get(possibility)
+				value := cf.Get(possibility)
+				assert.InDelta(expectedValue, value, tolerance)
+			}
+			assert.True(cf.Valid())
+		})
+	}
+}
+
 func BenchmarkCombineConjunctive(b *testing.B) {
 	mf1 := &MassFunction{}
 	mf1.Set(K(), 0.0)
@@ -492,5 +756,44 @@ func BenchmarkCombineDisjunctive(b *testing.B) {
 
 	for n := 0; n < b.N; n++ {
 		CombineDisjunctive(mfns...)
+	}
+}
+
+func BenchmarkCombineMurphyAverage(b *testing.B) {
+	mf1 := &MassFunction{}
+	mf1.Set(K(), 0.0)
+	mf1.Set(K("a"), 0.30)
+	mf1.Set(K("b"), 0.20)
+	mf1.Set(K("c"), 0.10)
+	mf1.Set(K("a", "b", "c"), 0.40)
+
+	mf2 := &MassFunction{}
+	mf2.Set(K("a"), 0.00)
+	mf2.Set(K("b"), 0.90)
+	mf2.Set(K("c"), 0.10)
+	mf2.Set(K("a", "b", "c"), 0.00)
+
+	mf3 := &MassFunction{}
+	mf3.Set(K("a"), 0.60)
+	mf3.Set(K("b"), 0.10)
+	mf3.Set(K("c"), 0.10)
+	mf3.Set(K("a", "b", "c"), 0.20)
+
+	mf4 := &MassFunction{}
+	mf4.Set(K("a"), 0.70)
+	mf4.Set(K("b"), 0.10)
+	mf4.Set(K("c"), 0.10)
+	mf4.Set(K("a", "b", "c"), 0.10)
+
+	mf5 := &MassFunction{}
+	mf5.Set(K("a"), 0.70)
+	mf5.Set(K("b"), 0.10)
+	mf5.Set(K("c"), 0.10)
+	mf5.Set(K("a", "b", "c"), 0.10)
+
+	mfns := []*MassFunction{mf1, mf2, mf3, mf4, mf5}
+
+	for n := 0; n < b.N; n++ {
+		CombineMurphyAverage(mfns...)
 	}
 }
